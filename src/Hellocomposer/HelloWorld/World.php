4d8a85c27cb109c224042e7b08bdc0dbe33bb91d
xionggang <xionggang@126.com>
2017-08-27 21:32:11 +0800

    hello

---
+++ src/Hellocomposer/HelloWorld/World.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * Author: xionggang  <1024236349@qq.com>
+ * Date: 17/8/27
+ * Time: 下午9:27
+ */
+namespace Hellocomposer\HelloWorld;
+
+class Hello
+{
+	public static function info()
+	{
+		echo "hello world xiong";
+	}
+}
